aa4b1638797fdbd0d4ec149733d0c6a9a5f79370
Braden Potter <71603976+BradenPotter@users.noreply.github.com>
2023-08-21 14:31:05 -0400

    Updated css and improved js

--- script.js
+++ script.js
@@ -6,13 +6,19 @@ function rollDice() {
             let sides = document.getElementById("side" + (i+1)).value;
             if (sides<1) {
                 sides = -1;
-            } 
+            }
             let roll = Math.floor(Math.random() * (sides) + 1);
-            total += roll;
-            document.getElementById("output" + (i+1)).innerHTML = roll;
+            // total += (roll + document.getElementById("addition" + (i+1)).value);
+            addition = document.getElementById("addition" + (i+1)).value;
+            total += Number(roll) + Number(addition);
+            rollTotal = Number(roll) + Number(addition);
+            document.getElementById("output" + (i+1)).innerHTML = roll + " + " + addition;
         }
     console.log(total);
-    document.getElementById("total").innerHTML = total;
+    if (total < 1) {
+        total = 1;
+    }
+    document.getElementById("total").innerHTML = "Total: " + total;
 }
 
 function addDice() {
@@ -25,14 +31,26 @@ function addDice() {
     div.setAttribute('id', 'dice' + numOfDice);
 
     // Create Text Node
-    node = document.createTextNode("Dice " + numOfDice +  " number of Sides: ")
+    node = document.createTextNode("Sides: ")
     div.appendChild(node);
-
 
     // Create Number of Sides input
     node = document.createElement("input");
     node.setAttribute('type', 'number');
+    node.setAttribute('min', '1');
+    node.setAttribute('value', '1');
     node.setAttribute('id', 'side' + numOfDice);
+    div.appendChild(node);
+
+    // Create Text Node
+    node = document.createTextNode(" + ");
+    div.appendChild(node);
+
+    // Create Number of Addition input
+    node = document.createElement("input");
+    node.setAttribute('type', 'number');
+    node.setAttribute('value', '0');
+    node.setAttribute('id', 'addition' + numOfDice);
     div.appendChild(node);
 
     // Create Text Node
@@ -54,4 +72,24 @@ function removeDice() {
     let dice = document.getElementById('dice' + numOfDice);
     dice.remove()
     numOfDice--;
+}
+
+var current = "light";
+var r = document.querySelector(':root');
+function darkMode() {
+    if (current === "light") {
+        r.style.setProperty('--headerfootercolor', '#071217');
+        r.style.setProperty('--backgroundcolor', '#121a1f');
+        r.style.setProperty('--text', '#f2f2f2');
+        document.getElementById("themeChanger").src = "/Images/Sun.png";
+        current = "dark";
+        console.log("something");
+        } else {
+        r.style.setProperty('--headerfootercolor', '#263740');
+        r.style.setProperty('--backgroundcolor', '#f2f2f2');
+        r.style.setProperty('--text', 'black');
+        document.getElementById("themeChanger").src = "/Images/Moon.png"
+        current = "light";
+        console.log("else");
+        };
 }
